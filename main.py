f2a3e906d7482a29ad8dbdafe41f2ab8e0362460
Breno Doria <breno@neoadtech.com.br>
2026-03-19 11:21:27 -0300

    battle cycle and loot2

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
-import pyautogui
+import pyautogui as pg
+import time
+import keyboard
+
+pg.useImageNotFoundException()
+REGION_BATTLE = (1193, 413, 172, 55)
+REGION_LOOT = (499, 231, 138, 137)
+
+# desabilita UI tibia, para fins de claridade visual do bot
+# pg.hotkey('ctrl', 'n')
+
+def check_battle():
+    return pg.locateOnScreen("imgs/region_battle.PNG", region=REGION_BATTLE)
+
+# time.sleep(2)
+# battle = check_battle()
+# print(battle)
+
+def kill_monsters():
+    while True:
+        # wait 0.5s
+        get_loot()
+        print("ta no whi le de kirk monsters")
+        keyboard.wait("h")
+        try:
+            battle = check_battle()
+            print(battle)
+        except pg.ImageNotFoundException:
+            pg.press("space")
+            while True:
+                try:
+                    pg.locateOnScreen("imgs/red_target.PNG", confidence=0.95, region=REGION_BATTLE)
+                    print("esperando o monstro morrer")
+                except pg.ImageNotFoundException:
+                    print("paro de ataca zé")
+                    break
 
 
-print(pyautogui.position())
-pyautogui.moveTo(763, 284, 3)
-pyautogui.screenshot('teste.png')
+def get_loot_old():
+    loot = pg.locateAllOnScreen("imgs/dead_monster.PNG", confidence=0.9, region=REGION_LOOT)
+    for box in loot:
+        x, y = pg.center(box)
+        pg.moveTo(x, y)
+        pg.click(button="right")
+
+def get_loot():
+    pg.hotkey('alt', 'q')
+
+kill_monsters()
